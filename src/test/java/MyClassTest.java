c98458544369734c48299783cf964391033299a3
iamkeerthisdpt <160541883+iamkeerthisdpt@users.noreply.github.com>
2024-07-14 17:53:06 -0700

    Update MyClassTest.java
    chromedriver path

--- src/test/java/MyClassTest.java
+++ src/test/java/MyClassTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -11,8 +13,23 @@ public class MyClassTest {
 @Test
 	public void myMethod()
 	{
-		WebDriverManager.chromedriver().setup();
+		/*WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.guru99.com/v4/");
+		driver.close();*/
+
+		WebDriverManager.chromedriver().setup();
+
+		
+		// Set path to Chromedriver binary
+       		 System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+       		 // Set path to Chrome binary
+		ChromeOptions options = new ChromeOptions();
+        	options.setBinary("/usr/bin/google-chrome");
+        	options.addArguments("--headless"); // Run Chrome in headless mode
+		ChromeDriver driver = new ChromeDriver(options);	
 		driver.manage().window().maximize();
 		driver.get("https://demo.guru99.com/v4/");
 		driver.close();
